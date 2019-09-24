f471295871f4e1267143504e845bd0c2c2db0c60
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 19:04:45 +0700

    Initial commit

---
+++ src/com/pbo/PBO11K10118902Latihan10Logika.java
@@ -0,0 +1,21 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * 
+ */
+
+public class PBO11K10118902Latihan10Logika {
+
+    public static void main(String[] args) {
+	    int umur = 19;
+	    boolean dewasa;
+	    dewasa = (umur >= 17);
+
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+}
